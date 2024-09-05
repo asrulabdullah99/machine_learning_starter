673d42828f038e183255d8bf0d3a932d2af9e54c
Asrul Abdullah <srulabdullah@gmail.com>
2024-09-05 21:40:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,7 @@ with st.expander('Input features'):
   st.write('**(combined penguins data)**')
   input_penguins
 #input_df
+
+#Encode
+encode = ['island','sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
